2f90c49a219bebca8aeb7449df189d4abb5cc58f
Anirudhsinh <mrhpotter124@gmail.com>
2025-06-22 21:30:26 +0530

    update

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -3,7 +3,7 @@ import codeImg from '../assets/code.jpg'
 import { checklistItems } from "../constants";
 
 const Workflow = () => {
-  return (
+  return ( 
     <div className="mt-20" id="workflow">
         <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide mb-6">
             Accelerate your <span className="bg-gradient-to-r from-blue-500 to-blue-800 text-transparent bg-clip-text">business workflow.</span>
